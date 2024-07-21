a4ded0e9c5ce6466fd31e13f0b583c0c9197cdc3
5nkolosite@gmail.com <cmcreators237@gmail.com>
2024-07-21 23:51:39 +0100

    added some functions

--- script.js/doctor/doctorprofile.js
+++ script.js/doctor/doctorprofile.js
@@ -5,50 +5,50 @@ const patientid = window.location.search.split("=")[1];
 console.log(patientid);
 const supabaseUrl = "https://pooghdwrsjfvcuagtcvu.supabase.co";
 const supabaseKey =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvb2doZHdyc2pmdmN1YWd0Y3Z1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEzMjYyNTAsImV4cCI6MjAzNjkwMjI1MH0.F7QURC-4NdgaGi82WGYAZ5r3m5UYVRCLwDAMS9Uc7vs";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvb2doZHdyc2pmdmN1YWd0Y3Z1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEzMjYyNTAsImV4cCI6MjAzNjkwMjI1MH0.F7QURC-4NdgaGi82WGYAZ5r3m5UYVRCLwDAMS9Uc7vs";
 
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.3/+esm";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 async function getDoctorDetails() {
-	try {
-		const { data, error } = await supabase
-			.from("users")
-			.select("*")
-			.eq("id", logUser);
-		if (error) {
-			console.log(error);
-		}
-		const id = data[0].id;
-		profileDetails(data);
-	} catch (error) {
-		console.error(error);
-	}
+  try {
+    const { data, error } = await supabase
+      .from("users")
+      .select("*")
+      .eq("id", logUser);
+    if (error) {
+      console.log(error);
+    }
+    const id = data[0].id;
+    profileDetails(data);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 getDoctorDetails();
 function profileDetails(user) {
-	const getStoreUsers = JSON.parse(localStorage.getItem("id"));
-	const userType = getStoreUsers.find((user) => user.id === logUser);
-	let className;
-	if (userType.type === "doctor") {
-		className = "btn";
-	} else {
-		className = "edit-btn";
-	}
-	console.log(className);
+  const getStoreUsers = JSON.parse(localStorage.getItem("id"));
+  const userType = getStoreUsers.find((user) => user.id === logUser);
+  let className;
+  if (userType.type === "doctor") {
+    className = "btn";
+  } else {
+    className = "edit-btn";
+  }
+  console.log(className);
 
-	const profileContainer = document.querySelector(".profile-content");
-	console.log("if user id is a string", className);
+  const profileContainer = document.querySelector(".profile-content");
+  console.log("if user id is a string", className);
 
-	profileContainer.innerHTML = ` <div class="profile-header">
+  profileContainer.innerHTML = ` <div class="profile-header">
   <span class ='premium'> <i class="fas fa-crown"></i></span>
  
                               <div class="profile-image">
                                    <img src="${
-																			user[0].userAvatar ||
-																			"https://shorturl.at/8TClo"
-																		}" alt="User Avatar">
+                                     user[0].userAvatar ||
+                                     "https://shorturl.at/8TClo"
+                                   }" alt="User Avatar">
                               </div>
                               <div >
                                   <h2 class="drName">Dr.${user[0].name}</h2>
@@ -56,15 +56,15 @@ function profileDetails(user) {
                               <div class ='action-btn'>
                                  
                                   <a href="/pages/editDoctorProfile.html?id=${
-																		user[0].id
-																	}" class="${className}">Edit Profile</a>
+                                    user[0].id
+                                  }" class="${className}">Edit Profile</a>
 
 
-                                  <a href="/pages/appointment.html?id=${
-																		user[0].id
-																	}" class="${
-		userType.type === "doctor" ? "edit-btn" : "btn"
-	}" style="margin-left: 1rem;">Book Appointments</a>
+                                 <div class="${
+                                   userType.type === "doctor"
+                                     ? "edit-btn"
+                                     : "btn"
+                                 }" style="margin-left: 1rem;">Book Appointments</div>
     <a class= "btn  upgradeBtn"  href="/pages/payment.html" style="margin-left: 1rem;">Upgrade</a></div>
 </div>
   
@@ -73,9 +73,9 @@ function profileDetails(user) {
                               <section class="section">
                                   <h3>About Dr. ${user[0].name}</h3>
                                   <p class = 'doctorBio' >${
-																		user[0].bio ||
-																		"Please add About you so that people can i know more about"
-																	}</p>
+                                    user[0].bio ||
+                                    "Please add About you so that people can i know more about"
+                                  }</p>
                               </section>
               
                               <div class="stats">
@@ -99,39 +99,45 @@ function profileDetails(user) {
                                   <h3>Contact Information</h3>
                                   <div class="contact-info">
                                       <p><strong>Email:</strong> ${
-																				user[0].email
-																			}</p>
+                                        user[0].email
+                                      }</p>
                                       <p><strong>Phone:</strong> ${
-																				user[0].phone
-																			}</p>
+                                        user[0].phone
+                                      }</p>
                                        <p><strong>Languages:</strong> ${
-																					user[0].languages
-																				}</p>
+                                         user[0].languages
+                                       }</p>
                                      
                                   </div>
                               </section>
               
                            
                           </div>`;
-	const premium = document.querySelector(".premium");
-	const upgradeBtn = document.querySelector(".upgradeBtn");
-	async function checkUserPaySatus() {
-		premium.style.display = "none";
-		upgradeBtn.style.display = "none";
-		const { data, error } = await supabase
-			.from("payment_history")
-			.select("*")
-			.eq("user_id", logUser);
+  const btn = document.querySelector(".btn")
+  btn.addEventListener('click', function(e){
+	e.preventDefault();
+	console.log('event just happen here')
+  })
 
-		if (data.length !== 0 || patientid) {
-			premium.style.display = "block";
-			upgradeBtn.style.display = "none";
-		} else {
-			upgradeBtn.style.display = "block";
-		}
-		if (error) {
-			console.log("error", error);
-		}
-	}
-	checkUserPaySatus();
+  const premium = document.querySelector(".premium");
+  const upgradeBtn = document.querySelector(".upgradeBtn");
+  async function checkUserPaySatus() {
+    premium.style.display = "none";
+    upgradeBtn.style.display = "none";
+    const { data, error } = await supabase
+      .from("payment_history")
+      .select("*")
+      .eq("user_id", logUser);
+
+    if (data.length !== 0 || patientid) {
+      premium.style.display = "block";
+      upgradeBtn.style.display = "none";
+    } else {
+      upgradeBtn.style.display = "block";
+    }
+    if (error) {
+      console.log("error", error);
+    }
+  }
+  checkUserPaySatus();
 }
