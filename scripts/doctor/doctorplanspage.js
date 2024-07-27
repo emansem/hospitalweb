61774d9ed9d94e051a5a8611b0763adf90943ac2
cmcreators237 <cmcreators237@gmail.com>
2024-07-27 20:52:35 +0100

    added a option for doctor to add a new plan and also a plan page to display their plan

---
+++ scripts/doctor/doctorplanspage.js
@@ -0,0 +1,74 @@
+const supabaseUrl = "https://pooghdwrsjfvcuagtcvu.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvb2doZHdyc2pmdmN1YWd0Y3Z1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEzMjYyNTAsImV4cCI6MjAzNjkwMjI1MH0.F7QURC-4NdgaGi82WGYAZ5r3m5UYVRCLwDAMS9Uc7vs";
+
+// Import and initialize Supabase client
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.3/+esm";
+const supabase = createClient(supabaseUrl, supabaseKey);
+//selcet the plan wrapper to render on the web page.
+const plansWraper = document.querySelector('.plans-wrapper');
+
+// get all plan  and render them to the web page
+async function getAllPlans() {
+  
+  const { data, error } = await supabase.from("doctor__plans").select("*");
+  if (data && data.length !== 0) {
+    console.log("data receive from plan ", data);
+    const plans = data;
+    console.log('this is the plans',plans);
+    const filterPlans = plans.filter(plan =>plan.status ==='active');
+    renderDoctorPlans(filterPlans);
+    console.log('plans', filterPlans)
+    
+   
+    //show the plan types  length /total
+    
+  } else if (!data || data.length === 0) {
+    console.log("we couldnot get the data");
+    plansWraper.innerHTML = 'No data found , Add a new plan';
+  } else {
+    console.log("this is the error", error);
+  }
+  }
+  getAllPlans();
+
+  // render the plans on the web page  so that user can buy.
+  function renderDoctorPlans(plans){
+
+    plans.forEach(plan=> {
+        const plansInJson = plan.features
+        // supase return the data in form of json we convert into js
+        const plansF = JSON.parse(plansInJson);
+        console.log(plansF);
+        // get only the value of the data.
+        const plansAsObject = Object.values(plansF);
+        
+       
+     const planFeatues =  plansAsObject.map(item =>{
+        return `   <p class="planFeatues">
+                        <i class="fas fa-check-circle"></i>
+                        <span> ${item}</span>
+                      </p>`
+     });
+        console.log(planFeatues)
+        // console.log(plansMap.map(item=>item.join(',')));
+        plansWraper.innerHTML += `
+        
+                <div class="plan-item">
+                  <div class="plan-header">
+                    <h2 class="planName">${plan.type}</h2>
+                    <p class="planPrice">$${plan.amount}</p>
+                  </div>
+              <div class="plan-body--wrapper">
+                <div class="plan-body">
+                  
+                   ${planFeatues}
+                    
+                  </div>
+                  <a class="paynow" href="/pages/payment.html?id=${plan.id}">Pay Now</a>
+              </div>
+                  
+                </div>`
+        
+    });
+  }
--- scripts/doctor/plans.js
+++ scripts/doctor/plans.js
@@ -20,15 +20,20 @@ const closUpdateForm = document.querySelector(".closUpdateForm");
 const plansContainer = document.querySelector(".payment_methods-wrapper");
 const upadtePlanForm = document.querySelector(".updateForm");
 const addNewMethodOverlay = document.querySelector(".addNew__method--overlay");
+const openSucessForm = document.querySelector('#success');
 
 // create or add a new plan this is the function that will allow doctors to add new paynment plans we have pay once or monthly
 async function addnewPlan(plan) {
   const { data, error } = await supabase
-    .from("plan_types")
+    .from("doctor__plans")
     .insert([plan])
     .select("*");
   if (data && data.length !== 0) {
-    location.reload();
+    openSucessForm.classList.remove('closeForm');
+    addNewPlanForm.reset();
+    setTimeout(function(){
+      window.location.reload();
+    }, 1500)
     console.log("new plan data", data);
   } else {
     console.log("this is the error", error);
@@ -51,9 +56,9 @@ addNewPlanForm.addEventListener("submit", function(e) {
   const planName = addNewPlanForm.plantype.value;
 
   const amount = addNewPlanForm.amount.value;
-  const option1 = addNewPlanForm.option1.value;
-  const option2 = addNewPlanForm.option2.value;
-  const option3 = addNewPlanForm.option3.value;
+  const option1 = addNewPlanForm.optional1.value;
+  const option2 = addNewPlanForm.optional2.value;
+  const option3 = addNewPlanForm.optional3.value;
  
   const plan = {
     type: planName,
@@ -76,14 +81,15 @@ upadtePlanForm.addEventListener("submit", function(e) {
   e.preventDefault();
   const planName = upadtePlanForm.plantype.value;
   const amount = upadtePlanForm.amount.value;
-  const option1 = upadtePlanForm.option1.value;
-  const option2 = upadtePlanForm.option2.value;
-  const option3 = upadtePlanForm.option3.value;
+  const option1 = upadtePlanForm.optional1.value;
+  const option2 = upadtePlanForm.optional2.value;
+  const option3 = upadtePlanForm.optional3.value;
+  const status = upadtePlanForm.status.value;
   const plan = {
     type: planName,
     doctorId:doctorId,
     amount:amount,
-    status: 'active',
+    status: status,
     features : {
       option1:option1,
       option2:option2,
@@ -114,9 +120,9 @@ closUpdateForm.addEventListener("click", function(e) {
 
 // Render plans on the page with few details;
 
-function renderPlanTypes(plans) {
+function renderPlans(plans) {
   plansContainer.innerHTML = "";
-  plantypes.forEach((plan, index) => {
+  plans.forEach((plan, index) => {
     const planWrapper = document.createElement("div");
 
     planWrapper.classList.add("page-item");
@@ -124,14 +130,15 @@ function renderPlanTypes(plans) {
     planWrapper.innerHTML = `
        <div class="page-number">${index + 1}</div>
             <div class="page-name">${plan.type}</div>
-            <div class="${plan.status ==='active' ? 'status' : 'inactive'}"><span>${plantype.status}</span></div>
+            <div class="${plan.status ==='active' ? 'status' : 'inactive'}"><span>${plan.status}</span></div>
          <div  id=${plan.id} class="action-buttons">
             <button class="actionBtn edit-page">Update</button>
+             <a href ='' class="actionBtn edit-page">Preview</a>
            <button  class="actionBtn delete">Delete</button>
          </div>
        `;
     // we select the action buttons for each plan  and also get the id form the buttons;
-    const actionButtons = plantypeWrapper.querySelector(".action-buttons");
+    const actionButtons = planWrapper.querySelector(".action-buttons");
     getActionButtions(actionButtons);
 
     plansContainer.appendChild(planWrapper);
@@ -157,11 +164,13 @@ function getActionButtions(button) {
 // update , change the plan  or change the status to deactive;
 async function upadtePlan(id, plan_types) {
   const { data, error } = await supabase
-    .from("doctor_pllans")
+  .from("doctor__plans")
     .update(plan_types)
     .select("*")
     .eq("id", id);
-    location.reload();
+   setTimeout(function(e){
+     location.reload();
+   }, 100);
   if (error) {
     console.log("error", error);
   } else {
@@ -172,7 +181,7 @@ async function upadtePlan(id, plan_types) {
 // the delete the Delete the plan  from the  and front end
 
 async function deletePlan(id){
-    const {data, error} = await supabase.from('doctor_plans').delete().eq('id', id).select();
+    const {data, error} = await supabase.from("doctor__plans").delete().eq('id', id).select();
     location.reload();
     if(data){
         console.log(data);
@@ -201,18 +210,43 @@ async function getAllPlanTypes() {
 }
 getAllPlanTypes();
 
+
+// get all plan  and render them to the web page
+async function getAllPlans() {
+  document.querySelector('.paymentNumbers').innerHTML = 0;
+const { data, error } = await supabase.from("doctor__plans").select("*");
+if (data && data.length !== 0) {
+  console.log("data receive from plan ", data);
+  const plans = data;
+  console.log('this is the plans',plans);
+  renderPlans(plans)
+  
+ 
+  //show the plan types  length /total
+  document.querySelector('.paymentNumbers').innerHTML =data.length;
+} else if (!data || data.length === 0) {
+  console.log("we couldnot get the data");
+  plansContainer.innerHTML = 'No data found , Add a new plan';
+} else {
+  console.log("this is the error", error);
+}
+}
+getAllPlans();
+
 // seleect all plans types and render them into the slect filed.
 const PlanTypeSelectInput = document.getElementById('method-type');
+const updateMethod = document.querySelector('#updateMethod');
 console.log(PlanTypeSelectInput);
 
 // this is to get all plans types and display on the plan selct filed;
 function displayPlanTypes(plantype){
   const options = plantype.map(item=>{
-return `<option   value="${item.id}">${item.type}</option>`
+   return `<option   value="${item.type}">${item.type}</option>`
 
 
   })
   PlanTypeSelectInput.innerHTML = options;
-  console.log(PlanTypeSelectInput);
+
+  updateMethod.innerHTML = options;
 
 }
