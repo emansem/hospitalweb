22d4a58f070a7431c2078d6625841a2f67ac3208
cmcreators237 <cmcreators237@gmail.com>
2024-08-01 19:58:26 +0100

    added some withdrawal method

--- scripts/withdrawals/withdrawal.js
+++ scripts/withdrawals/withdrawal.js
@@ -11,20 +11,23 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 const loggedUser = JSON.parse(localStorage.getItem("activeId"));
 const setupAccountForm = document.querySelector(".setup-gateway");
 const withdrawForm = document.querySelector(".withdrawalForm");
-const withdrawalContainer = document.querySelector('.withdrawal-form');
+const withdrawalContainer = document.querySelector(".withdrawal-form");
 const methodInput = document.getElementById("method");
-const setupGateWay = document.querySelector('.setupGateWay');
-const setupGateBtns = document.querySelector('.gateaway__form--btn');
-const withdrawalDetails = document.querySelector('.withdrawal__account--infor');
+const setupGateWay = document.querySelector(".setupGateWay");
+const setupGateBtns = document.querySelector(".gateaway__form--btn");
+const withdrawalDetails = document.querySelector(".withdrawal__account--infor");
 const setupGateWrapper = document.querySelector(".withdrawal__setup--gateway");
-const setupGateContainer =document.querySelector('.setupGateWay');
-const warn = document.querySelector('.headings-gateway');
-const success = document.querySelector('.success');
-const withdrawlaDetailsContainer = document.querySelector('.withdrawal__account--infor');
+const setupGateContainer = document.querySelector(".setupGateWay");
+const warn = document.querySelector(".headings-gateway");
+const success = document.querySelector(".success");
+const withdrawlaDetailsContainer = document.querySelector(
+	".withdrawal__account--infor"
+);
 console.log(withdrawForm);
-const withdrawalMethodSelect = document.querySelector('#withdrawalMethod');
-const withdrawalButtons = document.querySelector('.withdrawalButtons');
-const balance = document.querySelector('.balance');
+const withdrawalMethodSelect = document.querySelector("#withdrawalMethod");
+const withdrawalButtons = document.querySelector(".withdrawalButtons");
+const balance = document.querySelector(".balance");
+const withdrawalReports = document.querySelector(".withdrawal__reports");
 //get the payment methods and add to the select input.
 
 async function getAllPaymentMethods() {
@@ -51,16 +54,15 @@ async function checkIfDoctorHaveAccount() {
 		.select("*")
 		.eq("doctorid", loggedUser);
 	if (data && data.length !== 0) {
-		withdrawalDetails.id = '';
-       setupGateWrapper.classList.add('hideSetupAccount');
-        return;
+		withdrawalDetails.id = "";
+		setupGateWrapper.classList.add("hideSetupAccount");
+		return;
 	} else {
 		console.log("you have no data", data);
-      if(setupGateWrapper){
-        setupGateWrapper.classList.remove('hideSetupAccount');
-      
-      }
-        return;
+		if (setupGateWrapper) {
+			setupGateWrapper.classList.remove("hideSetupAccount");
+		}
+		return;
 	}
 }
 checkIfDoctorHaveAccount();
@@ -74,88 +76,87 @@ function collectUserInput() {
 		name: setupAccountForm.name.value,
 		doctorid: loggedUser,
 	};
-    if(accounDetails.phone === '' || accounDetails.pin === '' || accounDetails.name ===''){
-        warn.innerHTML ='All fields are required';
-        return;
-    }if(accounDetails.pin.length > 5 || accounDetails.pin.length < 5){
-        warn.innerHTML ='Your pin code must be 5 digits';   
-    }
-    
-    else{
-        console.log(accounDetails);
-        saveWithdrawalDetails(accounDetails);
-    }
-  
-
+	if (
+		accounDetails.phone === "" ||
+		accounDetails.pin === "" ||
+		accounDetails.name === ""
+	) {
+		warn.innerHTML = "All fields are required";
+		return;
+	}
+	if (accounDetails.pin.length > 5 || accounDetails.pin.length < 5) {
+		warn.innerHTML = "Your pin code must be 5 digits";
+	} else {
+		console.log(accounDetails);
+		saveWithdrawalDetails(accounDetails);
+	}
 }
 
 //send the form inputs  on the web server.
-async function saveWithdrawalDetails(accounDetails){
-    const { data, error } = await supabase
+async function saveWithdrawalDetails(accounDetails) {
+	const { data, error } = await supabase
 		.from("gateway_setup")
 		.insert([accounDetails])
-        .select();
+		.select();
 	if (data && data.length !== 0) {
-		console.log('this is the withdrawal account details here ', data);
+		console.log("this is the withdrawal account details here ", data);
 	} else {
 		console.log("you have no data", data);
 	}
-    if(error){
-        console.error(error);
-    }
+	if (error) {
+		console.error(error);
+	}
 }
 
 //add a event to open the form.
-setupGateWrapper.addEventListener('click', function(e){
- const displaySetGateForm =   e.target.textContent;
- if(displaySetGateForm ==='Add now'){
-   setupGateContainer.classList.remove('hide-setup-popup');
- }
-})
+setupGateWrapper.addEventListener("click", function (e) {
+	const displaySetGateForm = e.target.textContent;
+	if (displaySetGateForm === "Add now") {
+		setupGateContainer.classList.remove("hide-setup-popup");
+	}
+});
 
 //set even to close theform and open theform
-setupGateBtns.addEventListener('click', function(e){
-    const btnContent =  e.target.textContent;
-    if(btnContent === 'Cancel'){
-        setupGateContainer.classList.add('hide-setup-popup');
-        return
-    }
-     else if(btnContent === 'Add Now' ){
-        collectUserInput();
-        setupAccountForm.reset()
-        setTimeout(function(){
-            setupGateContainer.classList.add('hide-setup-popup');
-        },500)
-        setTimeout(function(){
-            success.classList.remove('hideForm');
-        },1500)
-        
-        return;
-        
-    }
+setupGateBtns.addEventListener("click", function (e) {
+	const btnContent = e.target.textContent;
+	if (btnContent === "Cancel") {
+		setupGateContainer.classList.add("hide-setup-popup");
+		return;
+	} else if (btnContent === "Add Now") {
+		collectUserInput();
+		setupAccountForm.reset();
+		setTimeout(function () {
+			setupGateContainer.classList.add("hide-setup-popup");
+		}, 500);
+		setTimeout(function () {
+			success.classList.remove("hideForm");
+		}, 1500);
+
+		return;
+	}
 });
 //add event to the success form
-success.addEventListener('click', function(e){
-    const hideForm  = e.target.textContent;
-    if(hideForm === 'Cancel'){
-        success.classList.add('hideForm');
-        setTimeout(function(){
-       location.reload();
-        }, 1000)
-    }
-})
+success.addEventListener("click", function (e) {
+	const hideForm = e.target.textContent;
+	if (hideForm === "Cancel") {
+		success.classList.add("hideForm");
+		setTimeout(function () {
+			location.reload();
+		}, 1000);
+	}
+});
 
 //render withdrawal withdrawal details on the web page
-function renderWithdrawalDetails(withdrawalDetails){
-    const phone = withdrawalDetails[0].phone;
-    
-  const phoneNum =    phone.toString().split('');
+function renderWithdrawalDetails(withdrawalDetails) {
+	const phone = withdrawalDetails[0].phone;
 
-  const formate1 = phoneNum.slice(0,3).join("")
-  const formate2 = phoneNum.slice(5, 8).join('')
-  withdrawalMethodSelect.innerHTML = `<option value="${phone}">${withdrawalDetails[0].method}---${formate1}xxx${formate2}</option>`
-    
-    withdrawlaDetailsContainer.innerHTML =`<p class="importport--notice">Withdrawal Account Details</p>
+	const phoneNum = phone.toString().split("");
+
+	const formate1 = phoneNum.slice(0, 3).join("");
+	const formate2 = phoneNum.slice(5, 8).join("");
+	withdrawalMethodSelect.innerHTML = `<option value="${phone}">${withdrawalDetails[0].method}---${formate1}xxx${formate2}</option>`;
+
+	withdrawlaDetailsContainer.innerHTML = `<p class="importport--notice">Withdrawal Account Details</p>
 
 								<div  class="withdrawal__account--items ">
 									<div class="withdrawal__items">
@@ -178,42 +179,144 @@ function renderWithdrawalDetails(withdrawalDetails){
 									<div>
 										<button class="withdrawalBtn ">Withdraw</button>
 									</div>
-								</div>`
-                                const withdrawalBtn  = document.querySelector('.withdrawalBtn');
-                                getWithdrawal(withdrawalBtn)
-}         
+								</div>`;
+	const withdrawalBtn = document.querySelector(".withdrawalBtn");
+	getWithdrawal(withdrawalBtn);
+}
 
 //get user withddrawal accoun details
-async function getWithdrawalAccountDetails(){
-    const {data, error} = await supabase.from('gateway_setup').select("*").eq('doctorid', loggedUser);
-    if(!data || data.length ===0){
-        console.log('we couldnot get your data sir');
-        return;
-    }else{
-        console.log('this is your data here boss', data);
-        renderWithdrawalDetails(data);
-    }
-    if(error){
-        console.error('You got and error here bro', error);
-    }
-
+async function getWithdrawalAccountDetails() {
+	const { data, error } = await supabase
+		.from("gateway_setup")
+		.select("*")
+		.eq("doctorid", loggedUser);
+	if (!data || data.length === 0) {
+		console.log("we couldnot get your data sir");
+		return;
+	} else {
+		console.log("this is your data here boss", data);
+		renderWithdrawalDetails(data);
+	}
+	if (error) {
+		console.error("You got and error here bro", error);
+	}
 }
 
 getWithdrawalAccountDetails();
 
 //get withddrawal details and addevent to save it.
 
-function getWithdrawal(btn){
-    btn.addEventListener('click', function(e){
-      withdrawalContainer.classList.remove('hide-withdrawal');
-    })
+function getWithdrawal(btn) {
+	btn.addEventListener("click", function (e) {
+		withdrawalContainer.classList.remove("hide-withdrawal");
+	});
 }
 
 //add event to the form to send and close.
- withdrawalButtons.addEventListener('click', function(e){
-    const withdrawaalBtn = e.target.textContent;
-   if(withdrawaalBtn ==='Cancel'){
-    withdrawalContainer.classList.add('hide-withdrawal');
-   }
- })
+withdrawalButtons.addEventListener("click", function (e) {
+	const withdrawaalBtn = e.target.textContent;
+	if (withdrawaalBtn === "Cancel") {
+		withdrawalContainer.classList.add("hide-withdrawal");
+        return
+	}else if(withdrawaalBtn ==='Withdraw'){
+        getWithdrawalInputDetails()
+    }
+});
 
+//render the reports on the web page
+
+async function renderReport(totalWithdrawAmount) {
+	const { data, error } = await supabase
+		.from("users")
+		.select("totalEarnings")
+		.eq("id", loggedUser);
+	console.log(data);
+
+	const currency = "XAF";
+
+	const formatCurrency = new Intl.NumberFormat("en-US", {
+		style: "currency",
+		currency: currency,
+	}).format(data[0].totalEarnings);
+	const totalWithdrawal = new Intl.NumberFormat("en-US", {
+		style: "currency",
+		currency: currency,
+	}).format(totalWithdrawAmount);
+	balance.innerHTML = formatCurrency;
+	localStorage.setItem("amount", data[0].totalEarnings);
+	console.log(formatCurrency);
+	withdrawalReports.innerHTML = `<div class="withdrawal__report--items">
+								<div class="withdrawal__reports--item">
+									<div class="report-contents">
+										<h3 class="reports-item-text">Total Balance</h3>
+										<span class="report__withdrawal--amount">${formatCurrency}</span>
+									</div>
+								</div>
+								<div class="withdrawal__reports--item">
+									<div class="report-contents">
+										<h3 class="reports-item-text">Total Withdrawal</h3>
+										<span class="report__withdrawal--amount">${totalWithdrawal}</span>
+									</div>
+								</div>
+							
+								
+							</div>`;
+}
+renderReport();
+
+//fetch the total withdrawal the doctor have made alredy
+
+async function getTotalWithdrawal() {
+	const { data, error } = await supabase
+		.from("withdrawals")
+		.select("*")
+		.eq("doctorid", loggedUser);
+	if (data || data.length !== 0) {
+		console.log("this is the data", data);
+		const totalWithdrawal = data.length;
+		renderReport(totalWithdrawal);
+	} else {
+		console.log("there is no data");
+	}
+	if (error) {
+		console.log("this is an error", error);
+	}
+}
+getTotalWithdrawal();
+
+//get withdrawal input details and store them
+
+function getWithdrawalInputDetails() {
+	const totalAmount = localStorage.getItem("amount");
+	const myBalance = Number(totalAmount);
+	const withdrawalInformation = {
+		method: withdrawForm.method.value,
+		pin: withdrawForm.pin.value,
+		amount: withdrawForm.amount.value,
+        doctorid:loggedUser,
+        description:'Withdrawal Was succesful'
+	};
+    if(withdrawalInformation.amount > myBalance){
+        alert('You Donot have insufficient Balance');
+        return
+    }else{
+createNewWithdrawalAccount(withdrawalInformation)
+    }
+}
+//insert a new withdrawal details in the withdrawal page
+
+async function createNewWithdrawalAccount(withddrawalDetails){
+    const { data, error } = await supabase
+		.from("gateway_setup")
+		.insert([withddrawalDetails])
+		.select();
+	if (data && data.length !== 0) {
+		console.log("this is the withdrawal  here ", data);
+        withdrawForm.reset();
+	} else {
+		console.log("you have no data", data);
+	}
+	if (error) {
+		console.error(error);
+	}
+}
