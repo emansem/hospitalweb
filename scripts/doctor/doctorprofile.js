372dfc25e8513e3093d8f342e2257d3e9ced2294
cmcreators237 <cmcreators237@gmail.com>
2024-08-03 16:45:53 +0100

    arrange the design for register account

--- scripts/doctor/doctorprofile.js
+++ scripts/doctor/doctorprofile.js
@@ -12,7 +12,7 @@ const doctorId = Number(queryString[1]);
 // Supabase configuration
 const supabaseUrl = "https://pooghdwrsjfvcuagtcvu.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvb2doZHdyc2pmdmN1YWd0Y3Z1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEzMjYyNTAsImV4cCI6MjAzNjkwMjI1MH0.F7QURC-4NdgaGi82WGYAZ5r3m5UYVRCLwDAMS9Uc7vs";
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvb2doZHdyc2pmdmN1YWd0Y3Z1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEzMjYyNTAsImV4cCI6MjAzNjkwMjI1MH0.F7QURC-4NdgaGi82WGYAZ5r3m5UYVRCLwDAMS9Uc7vs";
 
 // Import and initialize Supabase client
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.3/+esm";
@@ -25,51 +25,47 @@ import { showLoading } from "../custom_alert.js";
 
 // Function to fetch doctor details
 async function getDoctorDetails(conditon) {
-	showLoading()
-  const { data, error } = await supabase
-    .from("users")
-    .select("*")
-    .eq("id", logUser);
+	showLoading();
+	const { data, error } = await supabase
+		.from("users")
+		.select("*")
+		.eq("id", logUser);
 
-  const checkCurrentUserType = data[0].type;
+	const checkCurrentUserType = data[0].type;
 
+	if (checkCurrentUserType === "patient") {
+		const { data, error } = await supabase
+			.from("users")
+			.select("*")
+			.eq("id", doctorId);
 
-  if (checkCurrentUserType === "patient") {
-    console.log("hello");
-    const { data, error } = await supabase
-      .from("users")
-      .select("*")
-      .eq("id", doctorId);
+		renderDoctorProfileToUsers(data, conditon);
+	} else if (checkCurrentUserType === "doctor") {
+		const { data, error } = await supabase
+			.from("users")
+			.select("*")
+			.eq("id", logUser);
 
-    console.log("data", data);
-
-    renderDoctorProfileToUsers(data, conditon);
-  } else if (checkCurrentUserType === "doctor") {
-    console.log("data");
-    const { data, error } = await supabase
-      .from("users")
-      .select("*")
-      .eq("id", logUser);
-
-    // login user is doctor call this function
-    renderDoctorProfile(data);
-  }
+		// login user is doctor call this function
+		renderDoctorProfile(data);
+	}
 }
 
 getDoctorDetails();
 
 // Function to display profile details to users and hide somefunctions you donot want them to see
 function renderDoctorProfileToUsers(user, conditon) {
-  // Populate profile HTML
-  profileContainer.innerHTML = ` <div class="doctor__profile--wrapper">
+	// Populate profile HTML
+	profileContainer.innerHTML = ` <div class="doctor__profile--wrapper">
 							<div class="doctor__profile--header">
 									 <div class="header__left">
-									  <img  class="header__left--user-photo" src="${user[0].userAvatar ||
-                      "https://shorturl.at/8TClo"}" class="${user.name}">
+									  <img  class="header__left--user-photo" src="${
+											user[0].userAvatar || "https://shorturl.at/8TClo"
+										}" class="${user.name}">
 								  </div>
 								  <div class="heaer__right--userinfo">
 						          ${verifiedDoctor()}
-									  <span class="name">Dr.${user[0].name}</span>
+									  <span class="name">Dr .${user[0].name}</span>
 									  
 								  </div>
 							  </div>
@@ -82,8 +78,10 @@ function renderDoctorProfileToUsers(user, conditon) {
 					  <div class="profile-main">
 						<section class="section">
 						<h3>About Dr. ${user[0].name}</h3>
-						<p class='doctorBio'>${user[0].bio ||
-              "Please add About you so that people can i know more about"}</p>
+						<p class='doctorBio'>${
+							user[0].bio ||
+							"Please add About you so that people can i know more about"
+						}</p>
 						</section>
 						
 						<div class="stats">
@@ -111,30 +109,31 @@ function renderDoctorProfileToUsers(user, conditon) {
 						</section>
 					  </div>`;
 
-  // const btn = document.querySelector(".btn");
+	// const btn = document.querySelector(".btn");
 
-  // if (btn) {
-  // 	btn.addEventListener("click", function (e) {
-  // 		e.preventDefault();
-  // 		// reQuestPay();
-  // 	});
-  // }
+	// if (btn) {
+	// 	btn.addEventListener("click", function (e) {
+	// 		e.preventDefault();
+	// 		// reQuestPay();
+	// 	});
+	// }
 }
 
 // render profile todoctors only because it can conflict when user visit their profile
 
 function renderDoctorProfile(user) {
-  const userType = user[0].type;
+	// const userType = user[0].type;
 
-  profileContainer.innerHTML = ` <div class="doctor__profile--wrapper">
+	profileContainer.innerHTML = ` <div class="doctor__profile--wrapper">
 							<div class="doctor__profile--header">
 									 <div class="header__left">
-									  <img  class="header__left--user-photo" src="${user[0].userAvatar ||
-                      "https://shorturl.at/8TClo"}" class="${user.name}">
+									  <img  class="header__left--user-photo" src="${
+											user[0].userAvatar || "https://shorturl.at/8TClo"
+										}" class="${user.name}">
 								  </div>
 								  <div class="heaer__right--userinfo">
 						           ${verifiedDoctor()}
-									  <span class="name">Dr.${user[0].name}</span>
+									  <span class="name">Dr. ${user[0].name}</span>
 									  
 								  </div>
 							  </div>
@@ -147,8 +146,10 @@ function renderDoctorProfile(user) {
 					  <div class="profile-main">
 						<section class="section">
 						<h3>About Dr. ${user[0].name}</h3>
-						<p class='doctorBio'>${user[0].bio ||
-              "Please add About you so that people can i know more about"}</p>
+						<p class='doctorBio'>${
+							user[0].bio ||
+							"Please add About you so that people can i know more about"
+						}</p>
 						</section>
 						
 						<div class="stats">
@@ -175,118 +176,121 @@ function renderDoctorProfile(user) {
 						</div>
 						</section>
 					  </div>`;
-
-
 }
 
 // we have to get all the subscription for the patient and check.
 
 async function getPatientSubsciptions() {
-  const { data, error } = await supabase
-    .from("subscriptions")
-    .select("*")
-    .eq("patientid", logUser);
-  console.log("this is the patient sub", data);
+	const { data, error } = await supabase
+		.from("subscriptions")
+		.select("*")
+		.eq("patientid", logUser);
+	
 
-  if (data && data.length !== 0) {
-    const activeSubscription = data.filter(
-      subscription => subscription.pay_id !== null
-    );
-  
-    if (activeSubscription) {
-      allVariablesForToCompare(activeSubscription);
-    }
-  }else{
-	allVariablesForToCompare(data);
-  }
-  if (error) {
-    console.log("we got an error fetching the patient subscriptions", error);
-  }
+	if (data && data.length !== 0) {
+		const activeSubscription = data.filter(
+			(subscription) => subscription.pay_id !== null
+		);
+
+		if (activeSubscription) {
+			allVariablesForToCompare(activeSubscription);
+		}
+	} else {
+		allVariablesForToCompare(data);
+	}
+	if (error) {
+		console.log("we got an error fetching the patient subscriptions", error);
+	}
 }
 getPatientSubsciptions();
 
 //store all variables to check user plan and give them the access;
 
 function allVariablesForToCompare(subcription) {
-	if(subcription.length ===0){
+	if (subcription.length === 0) {
 		let contactBtb;
-				contactBtb = `<a href ='/pages/doctorplans.html?id=${doctorId}' class='btn' style="margin-left: 1rem;">Contact Me</a>`
-				getDoctorDetails(contactBtb);
-				
+		contactBtb = `<a href ='/pages/doctorplans.html?id=${doctorId}' class='btn' style="margin-left: 1rem;">Contact Me</a>`;
+		getDoctorDetails(contactBtb);
+
 		return;
-	}else{
+	} else {
 		const payOnce = "Pay per contact";
 		const monthlySubscription = "Monthly";
 		const typeOfSubscription = subcription[0].type;
 		const doctorSubsciptionId = Number(subcription[0].doctorid);
 		const next_pay_date = subcription[0].next_pay_date;
 		checkIfPatientBoughtAplan(
-		  payOnce,
-		  monthlySubscription,
-		  typeOfSubscription,
-		  doctorSubsciptionId,
-		  next_pay_date,
-		  subcription
+			payOnce,
+			monthlySubscription,
+			typeOfSubscription,
+			doctorSubsciptionId,
+			next_pay_date,
+			subcription
 		);
 	}
-
 }
 
 // check if the the patient have purchase a plan or not if he has, then check if the plan havae expired or doctor id is the doctor he subscribed and type plan he did purchase;
 
-	function checkIfPatientBoughtAplan(payOnce,monthlySubscription,typeOfSubscription,doctorSubsciptionId,next_pay_date, subcription) {
-	console.log('usersubscription', subcription)
-			if (
-				typeOfSubscription === payOnce &&
-				doctorSubsciptionId === doctorId &&
-				Date.now() < next_pay_date
-			) {
-				let contactBtb;
-				contactBtb = `<a href ='/pages/patientchatroom.html' class='btn' style="margin-left: 1rem;">Chat Now</a>`
-				getDoctorDetails(contactBtb);
-			} else if (
-				typeOfSubscription === monthlySubscription &&
-				doctorSubsciptionId === doctorId &&
-				Date.now() < next_pay_date
-			) {
-				let contactBtb;
-				contactBtb = `<a href ='/pages/patientchatroom.html' class='btn' style="margin-left: 1rem;">Chat Now</a>`
-				getDoctorDetails(contactBtb);
-				
-			}else if(subcription.length === 0){
-				alert('you donot have any plan with this doctos')
-			}
-			
-			else {
-				let contactBtb;
-				
-			   getDoctorDetails(`<a href ='/pages/doctorplans.html?id=${doctorId}' class='btn' style="margin-left: 1rem;">Contact Me</a>`);
-				
-				
-			}
-			}
+function checkIfPatientBoughtAplan(
+	payOnce,
+	monthlySubscription,
+	typeOfSubscription,
+	doctorSubsciptionId,
+	next_pay_date,
+	subcription
+) {
+	console.log("usersubscription", subcription);
+	if (
+		typeOfSubscription === payOnce &&
+		doctorSubsciptionId === doctorId &&
+		Date.now() < next_pay_date
+	) {
+		let contactBtb;
+		contactBtb = `<a href ='/pages/patientchatroom.html' class='btn' style="margin-left: 1rem;">Chat Now</a>`;
+		getDoctorDetails(contactBtb);
+	} else if (
+		typeOfSubscription === monthlySubscription &&
+		doctorSubsciptionId === doctorId &&
+		Date.now() < next_pay_date
+	) {
+		let contactBtb;
+		contactBtb = `<a href ='/pages/patientchatroom.html' class='btn' style="margin-left: 1rem;">Chat Now</a>`;
+		getDoctorDetails(contactBtb);
+	} else if (subcription.length === 0) {
+		alert("you donot have any plan with this doctos");
+	} else {
+		
+
+		getDoctorDetails(
+			`<a href ='/pages/doctorplans.html?id=${doctorId}' class='btn' style="margin-left: 1rem;">Contact Me</a>`
+		);
+	}
+}
 
 // load the doctor subscription and patients treated
 
-async function loadDoctorPatients(){
-	const {data, error} =  await supabase.from('subscriptions').select('*').eq('doctorid', logUser);
-	localStorage.setItem('patients', JSON.stringify(data));
-	
-	if(error){
-		console.log('we got an error', error);
+async function loadDoctorPatients() {
+	const { data, error } = await supabase
+		.from("subscriptions")
+		.select("*")
+		.eq("doctorid", logUser);
+	localStorage.setItem("patients", JSON.stringify(data));
+
+	if (error) {
+		console.log("we got an error", error);
 	}
 }
 loadDoctorPatients();
 
 //check if the doctor have 4 patients
 
-function verifiedDoctor(){
-	const subscriptions = JSON.parse(localStorage.getItem('patients'));
-	
-	 if(subscriptions.length >=4){
-		return `<span class="verify"> <i class="fa-solid fa-check-circle"></i> <span class="verify-text">Verified Doctor</span></span>`
-	}else{
-		return` <span class="verify notverified"> <i class="fas fa-times-circle"></i> <span class="verify-text">Not verified</span></span>`
+function verifiedDoctor() {
+	const subscriptions = JSON.parse(localStorage.getItem("patients"));
+
+	if (subscriptions.length >= 4) {
+		return `<span class="verify"> <i class="fa-solid fa-check-circle"></i> <span class="verify-text">Verified Doctor</span></span>`;
+	} else {
+		return ` <span class="verify notverified"> <i class="fas fa-times-circle"></i> <span class="verify-text">Not verified</span></span>`;
 	}
-	
 }
