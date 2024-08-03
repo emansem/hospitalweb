3328cffb609fee8c8c9b8a6fa8c5b8943a71c1c9
cmcreators237 <cmcreators237@gmail.com>
2024-08-03 18:54:41 +0100

    added error alerts on user profile

--- scripts/edituser.js
+++ scripts/edituser.js
@@ -1,64 +1,68 @@
 /** @format */
 import { previewimage } from "../scripts/data.js";
 const updateForm = document.getElementById("edit-profile-form");
-const logUser = JSON.parse(localStorage.getItem('activeId'));
-const loggedUser =logUser.id;
+const logUser = JSON.parse(localStorage.getItem("activeId"));
+import { showSucessAlert } from "../scripts/custom_alert.js";
+import { failedsAlert } from "../scripts/custom_alert.js";
+const loggedUser = logUser.id;
 console.log(loggedUser);
 
 const supabaseUrl = "https://pooghdwrsjfvcuagtcvu.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvb2doZHdyc2pmdmN1YWd0Y3Z1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEzMjYyNTAsImV4cCI6MjAzNjkwMjI1MH0.F7QURC-4NdgaGi82WGYAZ5r3m5UYVRCLwDAMS9Uc7vs";
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvb2doZHdyc2pmdmN1YWd0Y3Z1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEzMjYyNTAsImV4cCI6MjAzNjkwMjI1MH0.F7QURC-4NdgaGi82WGYAZ5r3m5UYVRCLwDAMS9Uc7vs";
 
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.3/+esm";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-
 previewimage();
 
 async function updateUserInLocalStorage(updatedUser) {
-  const { data, error } = await supabase
-    .from("users")
-    .update(updatedUser)
-    .eq("id", logUser);
-    alert("Profile updated successfully!");
-    // window.location.href ='/pages/userProfile.html'
-  if (error) console.log(error);
-  console.log(data);
+	const { data, error } = await supabase
+		.from("users")
+		.update(updatedUser)
+		.select()
+		.eq("id", logUser);
+	if (data && data.length !== 0) {
+		// alert("profile updated");
+		showSucessAlert("Profile updated sucessfully");
+    setTimeout(function(){
+      	window.location.href ='/pages/userProfile.html'
+    },1500)
+	} else {
+		failedsAlert("Something went, try gain¡");
+	}
+
+	if (error) console.log(error);
+	console.log(data);
 }
 
 function editUserProfile() {
-  const address = updateForm.address.value;
-  const dateOfBirth = updateForm.dateofbirth.value;
-  const imageFile = updateForm.profileimage.files[0];
+	const address = updateForm.address.value;
+	const dateOfBirth = updateForm.dateofbirth.value;
+	const imageFile = updateForm.profileimage.files[0];
 
-  if (imageFile) {
-    const reader = new FileReader();
-    reader.onload = function (event) {
-      const userAvatar = event.target.result;
-      document.getElementById("profile-image-preview").src =
-        event.target.result;
+	if (imageFile && dateOfBirth !== "" && address !== "") {
+		const reader = new FileReader();
+		reader.onload = function (event) {
+			const userAvatar = event.target.result;
+			document.getElementById("profile-image-preview").src =
+				event.target.result;
 
-      const updateUser = {
-        address: address,
-        userAvatar: userAvatar,
-        dateOfBirth: dateOfBirth,
-      };
+			const updateUser = {
+				address: address,
+				userAvatar: userAvatar,
+				dateOfBirth: dateOfBirth,
+			};
 
-      updateUserInLocalStorage(updateUser);
-    };
-    reader.readAsDataURL(imageFile);
-  } else {
-    // If no new image, update without changing the avatar
-    const updatedUser = {
-      address: address,
-      dateOfBirth: dateOfBirth,
-    };
-    updateUserInLocalStorage(updatedUser);
-  }
+			updateUserInLocalStorage(updateUser);
+		};
+		reader.readAsDataURL(imageFile);
+	} else {
+		failedsAlert("Please fill all the form!");
+	}
 }
 
 updateForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  editUserProfile();
-  
+	e.preventDefault();
+	editUserProfile();
 });
